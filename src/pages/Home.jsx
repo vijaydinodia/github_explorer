0af048f2e984bf9aaa4d6fdd154e39f6d9e259f8
Vijay Dinodia <147529376+vijaydinodia@users.noreply.github.com>
2026-06-28 23:46:50 +0530

    Add import for useRef in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+
 import useDebounce from "../hooks/useDebounce";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { searchUsers } from "../services/githubApi";
